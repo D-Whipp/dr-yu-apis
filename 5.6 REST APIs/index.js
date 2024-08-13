6ab27a57d5d3dfbb3c5bca3903cf7b22a88015fc
Mr Whipp <dwhipp88@gmail.com>
2024-08-12 18:25:35 -0600

    feature: route /post-secret - created logic to allow user to post additional secrets

--- 5.6 REST APIs/index.js
+++ 5.6 REST APIs/index.js
@@ -31,6 +31,7 @@ app.post('/get-secret', async (req, res) => {
       API_URL + '/secrets/' + searchId,
       config
     );
+    console.log('GET SECRET: ', result.data);
     res.render('index.ejs', { content: JSON.stringify(result.data) });
   } catch (error) {
     res.render('index.ejs', {
@@ -41,6 +42,20 @@ app.post('/get-secret', async (req, res) => {
 
 app.post('/post-secret', async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  const postSecret = req.body.secret;
+  const postScore = req.body.score;
+  try {
+    const result = await axios.post(
+      API_URL + '/secrets',
+      req.body,
+      config
+    );
+    res.render('index.ejs', { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render('index.ejs', {
+      content: JSON.stringify(error.response.data),
+    });
+  }
 });
 
 app.post('/put-secret', async (req, res) => {
