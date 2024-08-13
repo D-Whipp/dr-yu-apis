4d83488d5a77c0f94e50b68027d81ca182982d9d
Mr Whipp <dwhipp88@gmail.com>
2024-08-12 18:48:44 -0600

    feature: route /put-secret = created logic to allow user to edit their posted secret

--- 5.6 REST APIs/index.js
+++ 5.6 REST APIs/index.js
@@ -42,8 +42,6 @@ app.post('/get-secret', async (req, res) => {
 
 app.post('/post-secret', async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-  const postSecret = req.body.secret;
-  const postScore = req.body.score;
   try {
     const result = await axios.post(
       API_URL + '/secrets',
@@ -61,6 +59,18 @@ app.post('/post-secret', async (req, res) => {
 app.post('/put-secret', async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  try {
+    const result = await axios.put(
+      API_URL + '/secrets/' + searchId,
+      req.body,
+      config
+    );
+    res.render('index.ejs', { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render('index.ejs', {
+      content: JSON.stringify(error.response.data),
+    });
+  }
 });
 
 app.post('/patch-secret', async (req, res) => {
